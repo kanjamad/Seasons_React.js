3f6fd8051cee0c4a06f3cc0423c46523ed39d56b
kanjamad bosel <kanjamadpor@gmail.com>
2020-02-26 14:34:32 -0800

    add constructor

--- src/index.js
+++ src/index.js
@@ -2,18 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {lat: null, errorMessage: ''};
+
+        window.navigator.geolocation.getCurrentPosition(
+        position => {
+            this.setState({lat: position.coords.latitude});
+        },
+        err => {
+            this.setState({ errorMessage: err.message});
+        }
     );
-    
-    return(
+
+    }
+
+    render() {
+
+        return(
         <>
-            <div> Hi there!</div>
+            <div> 
+                Latitude: { this.state.lat}
+                <br />
+                Error: { this.state.errorMessage}
+            </div>
             <SeasonDisplay />
         </>
     );
-};
+    }
+}
+
 
 ReactDOM.render( <App />, document.querySelector('#root'))
