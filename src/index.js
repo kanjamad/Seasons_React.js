ff33cb37dbd04e96036393a8f11d77cc79be237b
kanjamad bosel <kanjamadpor@gmail.com>
2020-03-02 11:06:33 -0800

    used componentDidMount

--- src/index.js
+++ src/index.js
@@ -1,37 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
+// import SeasonDisplay from './SeasonDisplay';
 
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {lat: null, errorMessage: ''};
+    state = {lat: null, errorMessage: ''};
 
-        window.navigator.geolocation.getCurrentPosition(
-        position => {
-            this.setState({lat: position.coords.latitude});
-        },
-        err => {
-            this.setState({ errorMessage: err.message});
-        }
-    );
-
+    componentDidMount() {
+            window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({lat: position.coords.latitude}),
+            err => this.setState({ errorMessage: err.message})
+        );
     }
 
     render() {
 
-        return(
-        <>
-            <div> 
-                Latitude: { this.state.lat}
-                <br />
-                Error: { this.state.errorMessage}
-            </div>
-            <SeasonDisplay />
-        </>
-    );
+        if (this.state.errorMessage && !this.state.lat){
+        return <div> Error: {this.state.errorMessage}</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat){
+        return<div>Latitude: {this.state.lat}</div>
+        }
+
+        return <div> Loading!</div>;
+
     }
 }
 
