1526474d8abdf737a667a0fe83edde20044302b5
kanjamad bosel <kanjamadpor@gmail.com>
2020-03-03 14:37:13 -0800

    Showing Icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
+const getSeason = (lat, month) => {
+    if(month > 2 && month < 9){
+        return lat > 0 ? "summer" : "winter";
+    }else{
+        return lat < 0 ? "summer" : "winter";
+    }
+
+}
+
+const SeasonDisplay = props => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const text = season === "winter" ? "Burr, it is chilly" : "Let's hit the beach";
+    const icon = season === "winter" ? "snowflake":"sun";
+
     return (
-        <div> Seson Display</div>
+        <div> 
+            <i className={`${icon} icon`} />
+            <h1>{text}</h1>
+            <i className={`${icon} icon`} />
+        </div>
     );
 };
 
